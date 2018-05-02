890ef62b9630ff6ca71c5bb658f8e2a814c9a99b
federico1998 <fedecrave@hotmail.com>
2018-05-02 13:15:18 -0300

    paso de arduino a C

--- decremento/decremento.ino
+++ decremento/decremento.ino
@@ -1,128 +1,79 @@
-#include <DCMotor.h>
-#include <NewPing.h>
-DCMotor motor_izq(M0_EN,M0_D0,M0_D1,false);
-DCMotor motor_der(M1_EN,M1_D0,M1_D1);
-#define vel(a) motor_izq.setSpeed(a);motor_der.setSpeed(a);
+#include "mbed.h"
+#include "hcsr04.h"
 
-NewPing ultra_der (15,15,200);
-NewPing ultra_del (16,16,200);
-NewPing ultra_tra (19,19,200);
-float dist_der;
-float dist_del;
-float dist_tra;
-int velocidad;
-int luz = 130;
-#define f 6
-#define e 5
-#define g 11
-#define h 10
-#define p(a,b,c) analogWrite(a,c);analogWrite(b,c);
-#define l(z,x,y) digitalWrite(z,y);digitalWrite(x,y);
-void setup()
+PwmOut pwm1(PTC4);
+PwmOut pwm2(PTD3);
+//DigitalOut led(LED1);
+//DigitalOut led1(LED2);
+long dist_fder;
+long dist_fizq;
+float velocidad;
+const float razon = 1.25f;
+Serial pc(USBTX,USBRX);
+
+HCSR04 sonar_fizq(D6,D7);
+HCSR04 sonar_fder(D10,D11);
+
+
+float d_prom()
 {
-  //pinMode(14, INPUT); 
-  Serial.begin(9600);
-  pinMode(f,OUTPUT);
-  pinMode(e,OUTPUT);
-  pinMode(g,OUTPUT);
-  pinMode(h,OUTPUT);
-  delay(1000);
-  p(f,e,luz);
+    int dist_t = 0;
+    int cont = 0;
+    for(int i = 0; i <= 9; i++) {
+        dist_fizq = sonar_fizq.distance();
+        dist_fder = sonar_fder.distance();
+        wait(0.001f);
+
+        if(abs(dist_fder - dist_fizq) < 15) {
+            dist_t += dist_fizq + dist_fder;
+            cont += 2;
+        } else {
+            i--;
+        }
+    }
+    return(dist_t/cont);
 }
-void loop()
+void vel(int pot)
 {
-  d_prom();
-  if(dist_del > 100 || dist_del < 10)
-  {
-   vel(100); 
-   l(g,h,0);
-   p(f,e,luz);
-  }
-  else if(dist_del < 20)
-  {
-    vel(-3);
-    p(f,e,255);
-    l(g,h,1);
-    //while(true){}
-  }
-  else
-  {
-  velocidad = dist_del + 5;
-  vel(velocidad);
-  p(f,e,luz);
-  l(g,h,0);
-  delay(700);
-  }
+    if (pot >= 0) {
+    pwm1 = pot;
+    pwm2 = 0.0f;
+    }
+    else {
+        pwm2 = abs(pot)/100;
+        pwm1 = 0.0f;
+    }
+
 }
 
-
-void d_der()
+int main()
 {
-  float prom_der = 0;
+    pwm1.period_ms(5);
+    pwm2.period_ms(5);
+    
+    long dist;
+    while(1) {
+        dist = d_prom();
+        //printf("%l", dist);
+        if(dist >= 100.0f || dist == 0) {
+            vel(1.0f);
+        } else if(dist < 19.0f) {
+            vel(-0.9f);
+            wait(0.3);
+            vel(0.0f);
+        } else {
+            velocidad = (dist - 20.0f)*razon;
+            vel(velocidad);
+            printf("%.2f cm \n", dist);
+        }
+        /*
+                dist_fizq = sonar_fizq.getCm();
+                dist_fder = sonar_fder.getCm();
+                printf("%.2f cm   ", dist_fizq);
+                printf("%.2f cm   ", dist_fder);
+                printf("%.2f cm \n", (dist_fder+dist_fizq)/2);
+                wait(0.1f);
+        */
 
-  for(int i=1;i<=5;i++)
-  {
-    dist_der = ultra_der.ping_cm();
-    Serial.println(dist_der);
-    prom_der += dist_der;
-    delay(20);
-  }
-  Serial.println();
-  //dist_der = prom_der/5;
-  dist_der = prom_der/5;
-  return;
+    }
 }
-void d_del()
-{
-  float prom_del = 0;
-
-  for(int j=1;j<=4;j++)
-  {
-    dist_del = ultra_del.ping_cm();
-    Serial.println(dist_del);
-    prom_del += dist_del;
-    delay(30);
-  }
-  Serial.println();
-  //dist_der = prom_der/5;
-  dist_del = prom_del/4;
-  return;
-}
-void d_tra()
-{
-  float prom_tra = 0;
-
-  for(int k=1;k<=5;k++)
-  {
-    dist_tra = ultra_tra.ping_cm();
-    Serial.println(dist_tra);
-    prom_tra += dist_tra;
-    delay(20);
-  }
-  Serial.println();
-  //dist_der = prom_der/5;
-  dist_tra = prom_tra/5;
-  return;
-}
-
-
-////////////////////////////////////////////////////////
-void d_prom()
-{
-  float dist_t = 0;
-  int cont = 0;
-  for(int i = 0;i <= 7;i++)
-  {
-  dist_der = ultra_der.ping_cm();
-  delay(15);
-  dist_del = ultra_del.ping_cm();
-  delay(15);
-  if(abs(dist_del - dist_der) < 5)
-  {
-    dist_t += dist_del + dist_der;
-    cont += 2;
-  }
-  }
-  dist_del = dist_t/cont;
-}
-
